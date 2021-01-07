986bcfb33dc097bebd7f6d60a4c4130c481e4527
Kal Krueger <kkrueger19@gmail.com>
2021-01-07 10:10:43 -0700

    classes and objects_2

---
+++ RB120-129/classes_and_objects_2.rb/counting_cats.rb
@@ -0,0 +1,18 @@
+class Cat
+  @@total_cats = 0
+
+  def initialize
+    @@total_cats += 1
+  end
+
+  def self.total
+    puts @@total_cats
+  end
+
+end
+
+
+kitty1 = Cat.new
+kitty2 = Cat.new
+
+Cat.total
